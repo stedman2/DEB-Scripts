9ab9ed553807c8394eaf393db0d8b9e531e51e25
matt t <beefyj@gmail.com>
2012-11-30 21:39:30 +1100

    laalaala

--- ssh.sh
+++ ssh.sh
@@ -1,9 +1,8 @@
 # SSH Config Setup
-# also will need to rm -rf /etc/ssh/sshd_config first?
+# remove default config
 rm -rf /etc/ssh/sshd_config
-
-echo -e "#AutoGen on `date` by `whoami` Mod Nov 2012 sorta" >> /etc/ssh/sshd_config
-echo "Your port number for SSH LOGIN - REMEMBER THIS!! IMPORTANT!"
+echo -e "#AutoGen on `date` by `whoami` " >> /etc/ssh/sshd_config
+echo "Your port number for SSH LOGIN - REMEMBER 
 read portn
 echo "Login to SHH on port number: $portn "
 echo "Port $portn" >> /etc/ssh/sshd_config
@@ -13,7 +12,6 @@ read ipaddy
 echo "I.P Added"
 echo "Your email address"
 read emailaddy
-
 echo "ListenAddress $ipaddy" >> /etc/ssh/sshd_config
 echo "SyslogFacility AUTHPRIV" >> /etc/ssh/sshd_config
 echo "LoginGraceTime 60" >> /etc/ssh/sshd_config
@@ -28,10 +26,9 @@ echo "UsePAM yes" >> /etc/ssh/sshd_config
 echo "X11Forwarding no" >> /etc/ssh/sshd_config
 echo "UseDNS yes" >> /etc/ssh/sshd_config
 #echo "PermitRootLogin no" >> /etc/ssh/sshd_config
-# new 
 #echo "AllowUsers exampleuser exampleuser2" >> /etc/ssh/sshd_config
 echo "SSH Setup Completed!"
-#/etc/init.d/ssh restart
+/etc/init.d/ssh restart
 #service apache2 stop
 # Message Of The Day - On login
 echo "Welcome! :) " >> /etc/motd
