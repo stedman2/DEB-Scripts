e8831647b6dd2175cdb67ea3e65d4d91d3c438a9
matt t <beefyj@gmail.com>
2012-12-01 01:39:54 +1100

    depends

--- DEB-webmin.sh
+++ DEB-webmin.sh
@@ -2,7 +2,9 @@
 
 # update
 apt-get -y update && apt-get -y upgrade
-# install
+# install shiz needed
+apt-get -y install libnet-ssleay-perl libauthen-pam-perl libio-pty-perl apt-show-versions
+
 wget http://prdownloads.sourceforge.net/webadmin/webmin_1.610_all.deb
 #
 dpkg --install webmin_1.610_all.deb
